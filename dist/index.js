f60e53662bb7b36fcd52bc0ff0945137c027ee84
Jordan Hilado <jordanalihilado@gmail.com>
2022-01-30 15:48:03 -0800

    almost done w/ session authentication

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 const core_1 = require("@mikro-orm/core");
+const constants_1 = require("./constants");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
 const express_1 = __importDefault(require("express"));
 const apollo_server_express_1 = require("apollo-server-express");
@@ -16,15 +17,36 @@ const main = async () => {
     const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
     await orm.getMigrator().up();
     const app = (0, express_1.default)();
+    const { createClient } = require("redis");
+    const session = require("express-session");
+    let RedisStore = require("connect-redis")(session);
+    let redisClient = createClient();
+    app.use(session({
+        name: "qid",
+        store: new RedisStore({ client: redisClient, disableTouch: true }),
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+            httpOnly: true,
+            sameSite: "lax",
+            secure: constants_1.__prod__,
+        },
+        saveUninitialized: false,
+        secret: "sadjhfjksdhfjksdhf",
+        resave: false,
+    }));
     const apolloServer = new apollo_server_express_1.ApolloServer({
         schema: await (0, type_graphql_1.buildSchema)({
             resolvers: [hello_1.HelloResolver, post_1.PostResolver, user_1.UserResolver],
             validate: false,
         }),
-        context: () => ({ em: orm.em }),
+        context: ({ req, res }) => ({ em: orm.em, req, res }),
     });
     await apolloServer.start();
-    apolloServer.applyMiddleware({ app });
+    const cors = {
+        credentials: true,
+        origin: "https://studio.apollographql.com",
+    };
+    apolloServer.applyMiddleware({ app, cors });
     app.listen(4000, () => {
         console.log("server started on localhost:4000");
     });
--- src/index.ts
+++ src/index.ts
@@ -15,16 +15,43 @@ const main = async () => {
 
   const app = express();
 
+  const { createClient } = require("redis");
+  const session = require("express-session");
+
+  let RedisStore = require("connect-redis")(session);
+  let redisClient = createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({ client: redisClient, disableTouch: true }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: "lax", // csrf
+        secure: __prod__, // cookie only works in https
+      },
+      saveUninitialized: false,
+      secret: "sadjhfjksdhfjksdhf",
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    context: ({ req, res }) => ({ em: orm.em, req, res }),
   });
   await apolloServer.start();
 
-  apolloServer.applyMiddleware({ app });
+  const cors = {
+    credentials: true,
+    origin: "https://studio.apollographql.com",
+  };
+
+  apolloServer.applyMiddleware({ app, cors });
 
   app.listen(4000, () => {
     console.log("server started on localhost:4000");
