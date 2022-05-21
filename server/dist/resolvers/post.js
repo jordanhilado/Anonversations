a8183f9d9fc360d5c8bdb5e154c71c6171657a73
Jordan Hilado <jordanalihilado@gmail.com>
2022-05-21 00:12:58 -0700

    finished chakra styling

--- server/dist/resolvers/post.js
+++ server/dist/resolvers/post.js
@@ -31,6 +31,9 @@ PostInput = __decorate([
     (0, type_graphql_1.InputType)()
 ], PostInput);
 let PostResolver = class PostResolver {
+    textSnippet(root) {
+        return root.text.slice(0, 50);
+    }
     async posts(limit, cursor) {
         const realLimit = Math.min(50, limit);
         const qb = (0, typeorm_1.getConnection)()
@@ -64,6 +67,13 @@ let PostResolver = class PostResolver {
         return true;
     }
 };
+__decorate([
+    (0, type_graphql_1.FieldResolver)(() => String),
+    __param(0, (0, type_graphql_1.Root)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Post_1.Post]),
+    __metadata("design:returntype", void 0)
+], PostResolver.prototype, "textSnippet", null);
 __decorate([
     (0, type_graphql_1.Query)(() => [Post_1.Post]),
     __param(0, (0, type_graphql_1.Arg)("limit", () => type_graphql_1.Int)),
@@ -104,7 +114,7 @@ __decorate([
     __metadata("design:returntype", Promise)
 ], PostResolver.prototype, "deletePost", null);
 PostResolver = __decorate([
-    (0, type_graphql_1.Resolver)()
+    (0, type_graphql_1.Resolver)(Post_1.Post)
 ], PostResolver);
 exports.PostResolver = PostResolver;
 //# sourceMappingURL=post.js.map
